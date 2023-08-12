32318c9f93f059c83127793d2b93d53638698471
priyanshuchaudhary53 <priyanshuchaudhary53@gmail.com>
2023-08-12 15:22:59 +0530

    update Games.tsx

--- src/components/Games.tsx
+++ src/components/Games.tsx
@@ -18,7 +18,7 @@ export default function Games({ intialGames }: { intialGames: Game[] }) {
         fetching.current = true;
 
         const res = await fetch(
-          `https://${process.env.VERCEL_URL}/api/games?page=${page}`
+          `https://${process.env.DOMAIN}/api/games?page=${page}`
         );
         const data = await res.json();
 
